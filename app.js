bb8f84fb7fe6f584a5531db12a8bb3d63e341f5f
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-09-17 22:43:18 +0700

    install package, use dotenv to hide secret information, connect db use mongoose

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
 const port = 3000;
@@ -6,6 +8,21 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+// Connect to DB
+async function connectToDb() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.poo9ipk.mongodb.net/test`
+    );
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log("Connected to DB!");
+  }
+}
+
+connectToDb();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
